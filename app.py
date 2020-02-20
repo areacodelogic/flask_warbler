6caca6f54c12d1f5662d810e97d16e8eebbd1ecf
WinnieGotYu <WinnieGotYu@gmail.com>
2020-02-20 12:29:52 -0800

    imported SECRET_KEY

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from ignored import SECRET_KEY
 
 from flask import Flask, render_template, request, flash, redirect, session, g
 # from flask_debugtoolbar import DebugToolbarExtension
@@ -19,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', SECRET_KEY)
 # toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
